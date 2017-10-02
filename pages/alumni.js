60789331c27c74894f25106832eebfdff5ec30d5
Emmanuel Fleurine <emmanuelfleurine@gmail.com>
2017-10-01 20:58:18 -0400

    Refractor alumni to use only tachyons classes

--- pages/alumni.js
+++ pages/alumni.js
@@ -3,25 +3,29 @@ import BaseLayout from '../containers/BaseLayout';
 import Info from '../components/SchoolInfo';
 import AlumnusCard from '../components/AlumnusCard';
 
-export default () => (
-  <BaseLayout>
-    <Info/>
-    <div className="school-info-year tc " style={{margin: '2%', 'font-weight': 'bold','font-family': 'Helvetica', 'font-size': '1.5em'}}>2017</div>
-    <div className="mw9 center ph3-ns">
-      <div className="cf ph2-ns">
-        <div className="fl w-100 w-25-ns pa2">
-          <AlumnusCard />
-        </div>
-        <div className="fl w-100 w-25-ns pa2">
-          <AlumnusCard />
-        </div>
-        <div className="fl w-100 w-25-ns pa2">
-          <AlumnusCard />
-        </div>
-        <div className="fl w-100 w-25-ns pa2">
-          <AlumnusCard />
+function Alumni() {
+  return (
+    <BaseLayout>
+      <Info />
+      <div className="school-info-year tc helvetica b f3 ma3" >2017</div>
+      <div className="mw9 center ph3-ns">
+        <div className="cf ph2-ns">
+          <div className="fl w-100 w-25-ns pa2">
+            <AlumnusCard />
+          </div>
+          <div className="fl w-100 w-25-ns pa2">
+            <AlumnusCard />
+          </div>
+          <div className="fl w-100 w-25-ns pa2">
+            <AlumnusCard />
+          </div>
+          <div className="fl w-100 w-25-ns pa2">
+            <AlumnusCard />
+          </div>
         </div>
       </div>
-    </div>
-  </BaseLayout>
-);
+    </BaseLayout>
+  )
+}
+
+export default Alumni
